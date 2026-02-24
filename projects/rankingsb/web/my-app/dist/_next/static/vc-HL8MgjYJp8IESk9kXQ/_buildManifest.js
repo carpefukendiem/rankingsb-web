c9d799846c797689dcb0c34e3e4e11c3f2a4bd9d
carpefukendiem <rubenstips@gmail.com>
2026-02-24 14:40:58 -0800

    Autosave: 2026-02-24 14:40:58 - Workspace sync

---
+++ projects/rankingsb/web/my-app/dist/_next/static/vc-HL8MgjYJp8IESk9kXQ/_buildManifest.js
@@ -0,0 +1,15 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "static/chunks/cbb0481e71645fca.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error",
+    "/api/contact"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
